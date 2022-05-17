0386d5c41a48b61d023c80ef4d428779db75cc01
Igor Savenko <csigorek@gmail.com>
2022-05-16 17:27:52 -0700

    Added IntFixer

--- toolkit.py
+++ toolkit.py
@@ -6,11 +6,65 @@ from random import randint
 
 from fastapi import HTTPException, Header
 
+from toolkit_internal.exceptions import *
 
 class ToolKit:
     def __init__(self):
         pass
 
+
+    class IntFixer:
+        def __init__():
+            pass
+
+        
+        # Maybe later I try realize it with regex :)
+        async def RepresentsInt(
+            self, 
+            value,
+            return_value=False
+        ):
+            try: 
+
+                int(value)
+
+                if return_value:
+                    return int(value)
+                return True
+
+            except ValueError:
+                return False
+
+        
+        async def fix(
+            self,
+            value,
+            fix_negative=True,
+            fix_negative_only=False,
+            max_value=None,
+            min_value=None,
+        ):
+            if not self.RepresentsInt():
+                raise InvalidRepresentOfNumber
+            
+            if fix_negative:
+                if value < 0 and not 0:
+                    value = value * -1
+                
+            if fix_negative_only:
+                return value
+
+            if max_value:
+                if value > max_value:
+                    return max_value
+
+            if min_value:
+                if value < min_value:
+                    return min_value        
+            
+            return int(value)
+                        
+                        
     class Generate:
         def __init__(self):
             pass
@@ -20,13 +74,17 @@ class ToolKit:
                 return uuid.uuid4().hex
             return uuid.uuid4()
 
-        async def random_code(min=None, max=None):
+        async def random_code(
+            min=None,
+            max=None
+        ):
             if min and max:
                 return randint(min, max)
             return randint(1000, 9999)
         
         def list_selector(array):
             return secrets.choice(array)
+
 
     class FastJWT:
         def __init__(self):
@@ -35,9 +93,15 @@ class ToolKit:
         def set_secret_key(self, secret_key):
             self.secret_key = secret_key
 
-        async def encode(self, optional_data=None, expire=None):
+        async def encode(
+            self,
+            optional_data=None,
+            expire=None
+        ):
             if not expire:
-                expire = (datetime.datetime.now() + datetime.timedelta(days=30)).timestamp()
+                expire = (
+                    datetime.datetime.now() + datetime.timedelta(days=30)
+                ).timestamp()
 
             token_json = {
                 "expire": expire
@@ -46,13 +110,21 @@ class ToolKit:
             if optional_data:
                 token_json.update(optional_data)
 
-            jwt_token = jwt.encode(token_json, self.secret_key, algorithm="HS256")
+            jwt_token = jwt.encode(
+                token_json,
+                self.secret_key,
+                algorithm="HS256"
+            )
 
             return jwt_token
         
 
         async def decode(self, payload):
-            return jwt.decode(payload, self.secret_key, algorithms=["HS256"])
+            return jwt.decode(
+                payload,
+                self.secret_key,
+                algorithms=["HS256"]
+            )
 
 
         async def login_required(self, Authorization=Header("Authorization")):
